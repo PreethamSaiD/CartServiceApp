5c901c62a83211da7c0caf07b34ec2bb3176f915
PreethamSaiD <preetham1080@gmail.com>
2026-01-04 19:02:51 +0530

    Added two new mappings which are important in the GetMapping.

--- CartService/src/main/java/com/preetham/CartService/NumberController.java
+++ CartService/src/main/java/com/preetham/CartService/NumberController.java
@@ -1,6 +1,8 @@
 package com.preetham.CartService;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +13,14 @@ public class NumberController {
     public int hello(){
         return (int)(Math.random()*1000);
     }
-}
+        @GetMapping("/hello/{name}")
+        public String name(@PathVariable String name){
+            return "Hi "+ name+"!";
+        }
+
+        @GetMapping("/add")
+        public int add(@RequestParam int a,@RequestParam int b){
+        return a+b;
+        }
+    }
+
